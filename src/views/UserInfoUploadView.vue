ec37bac79c9f61025afef7a9319b954eb6a8b0ae
gdw118 <3295432269@qq.com>
2025-03-09 12:00:06 +0800

    fix: 修复多个文件中的组件解析错误或路径错误

--- src/views/UserInfoUploadView.vue
+++ src/views/UserInfoUploadView.vue
@@ -1,9 +1,9 @@
 <template>
   <section class="bg-white">
-    <second class="flex flex-col md:flex-row h-screen items-center">
+    <section class="flex flex-col md:flex-row h-screen items-center">
       <!-- 填写昵称、真实姓名、性别、学院、专业、QQ 账号、微信号 -->
       <div
-        class="bg-white w-full md:max-w-md lg:max-w-full md:mx-auto md:mx-0 md:w-1/2 xl:w-1/3 h-screen px-6 lg:px-16 xl:px-12 flex items-center justify-center"
+        class="bg-white w-full md:max-w-md lg:max-w-full md:mx-auto md:w-1/2 xl:w-1/3 h-screen px-6 lg:px-16 xl:px-12 flex items-center justify-center"
       >
         <div class="w-full h-100">
           <div>
@@ -132,7 +132,7 @@
           </p>
 
           <p class="text-sm text-gray-500 mt-12">
-            &copy; 2023 SHNU CoderSky - All Rights Reserved.
+            &copy; 2025 SHNU CoderSky - All Rights Reserved.
           </p>
         </div>
       </div>
@@ -144,7 +144,7 @@
           class="w-full h-full object-cover"
         />
       </div>
-    </second>
+    </section>
   </section>
 </template>
 
